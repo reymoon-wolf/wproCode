dca9a364c267baf0d8ada8c1b0f23b34cfb504ce
reymoon-wolf <156504100+reymoon-wolf@users.noreply.github.com>
2024-01-13 13:10:36 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// console.log('hello world')
+// const firstName = 'Andrew'
+// firstName = 'Andrey'
+// console.log('Hello', firstName)
+
+let firstName = 'Andrew'
+firstName = 'Andrey'
+
+console.log('Hello', firstName)
